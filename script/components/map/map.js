3a5be92f7f639ecafb5f95479e2abd9781cab3fa
Artem-Kalugin <Aharlo4@gmail.com>
2020-12-21 00:12:04 +0300

    fix: fix map render in chrome

--- script/components/map/map.js
+++ script/components/map/map.js
@@ -1,6 +1,6 @@
 function drawMap(center, DOMElement) {
   mapboxgl.accessToken = 'pk.eyJ1IjoiYXNpbmRldG9uIiwiYSI6ImNrM3l5eG1oYjBkNTAzbXAzb3N0OWVkcngifQ.9-7d9OiVIiV5KWubQm1KOQ';
-  return map = new mapboxgl.Map({
+  return new mapboxgl.Map({
     container: DOMElement?DOMElement:'map',
     style: 'mapbox://styles/mapbox/light-v10',
     zoom: 5,
