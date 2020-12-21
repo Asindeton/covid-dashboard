c6be9c10cbebd130fafe82c472cd5fc10234533a
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2020-12-22 00:39:48 +0300

    fix: per 100000 value and Dashboard method names

--- script/components/DashboardItems/GlobalDeathsItem.js
+++ script/components/DashboardItems/GlobalDeathsItem.js
@@ -13,14 +13,14 @@ export default class GlobalDeathsItem extends DashboardItem {
     super.updateItemInfo(data, state);
     const getNumber = (x) => (this.state.time === 'lastDay' ? x.todayDeaths : x.deaths);
     this.globalDeathsCountElement.innerHTML = numberFormatter(getNumber(this.data.Global),
-      this.state.population);
+      this.state.population, this.data.Global.population);
     this.itemContainer.innerHTML = '';
     sorter(this.data, getNumber);
     for (let i = 0; i < this.data.length; i += 1) {
       if (getNumber(this.data[i]) > 0
       && (!this.state.region || this.data[i].country === this.state.region)) {
         this.itemContainer.append((new GridItem('cases cases_death', numberFormatter(getNumber(this.data[i]),
-          this.state.population), this.data[i].country)).gridItem);
+          this.state.population, this.data[i].population), this.data[i].country)).gridItem);
       }
     }
   }
