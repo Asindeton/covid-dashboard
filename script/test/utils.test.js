bfa544e4ed968663baa141f8fccc45395c8a450e
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2020-12-23 22:35:21 +0300

    fix: fix tests

--- script/test/utils.test.js
+++ script/test/utils.test.js
@@ -1,26 +1,24 @@
-import { getMarkSize } from '../utils/mapUtils';
+import getCountryCode from '../utils/getCountryCode';
 import { numberFormatter, numberFormatterToString } from '../utils/formatter';
 import sort from '../utils/sorter';
 import DataService from '../components/DataService';
+import countries from '../components/Countries';
 
 test('getMarkSize', () => {
-  expect(getMarkSize(1)).toBe(26);
-  for (let i = 0; i < 1000; i += 10) {
-    expect(getMarkSize(i ** i)).toBeDefined();
-    expect(getMarkSize(i ** (1 / i))).toBeDefined();
-  }
+  expect(getCountryCode('Belarus', countries)).toBe('BY');
+  expect(getCountryCode('Russia', countries)).toBeDefined();
 });
 test('numberFormatter', () => {
   expect(numberFormatter(512312, 'formatted', 50000)).toBe(1024624);
   expect(numberFormatter(512312, 'formatted', 5000)).toBe(10246240);
   for (let i = 0; i < 1000; i += 10) {
-    expect(getMarkSize((i ** i), 'formatted', (i ** i))).toBeDefined();
-    expect(getMarkSize((i ** i), 'formatted', i ** (1 / i))).toBeDefined();
+    expect(numberFormatter((i ** i), 'formatted', (i ** i))).toBeDefined();
+    expect(numberFormatter((i ** i), 'formatted', i ** (1 / i))).toBeDefined();
   }
 });
 
 test('sort', () => {
-  expect(sort([1241, 1232, 52343, 1234, 56457, 52346, 7123, 8], getMarkSize, 50000)).toBeDefined();
+  expect(sort([1241, 1232, 543, 124, 56457, 52346, 7123, 8], numberFormatter, 50000)).toBeDefined();
 });
 
 test('DataService', () => {
