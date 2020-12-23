0fa36934e3f04a8f4e7cdfab110773784fe56f72
Artem-Kalugin <Aharlo4@gmail.com>
2020-12-23 23:01:59 +0300

    feat: add start alert

--- script/script.js
+++ script/script.js
@@ -4,3 +4,5 @@ import Dashboard from './components/Dashboard';
 
 const dashboard = new Dashboard();
 dashboard.generateDashboard();
+// eslint-disable-next-line no-alert
+alert('Привет. В таске реализован весь функционал. Подсветка клавиатуры в поиске работает только при совпадении состояния физической и вирутальной. Апи возвращает данные не для всех стран (особенно за последний день).')
